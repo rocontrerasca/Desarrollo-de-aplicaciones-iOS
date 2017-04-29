4c2b0a9522dc71fba899c67329b0845a155533e8
Richar Contreras <rocontrerasca@unal.edu.co>
2017-04-29 10:22:22 -0700

    Segundo curso agregado

---
+++ 2.Interfaz de usuario en iOS/PizzaExpress/PizzaExpress/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  PizzaExpress
+//
+//  Created by Richar on 29/04/17.
+//  Copyright © 2017 devSoft. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
