e598bb4057ff293cfdc852993d830445c70ab3cc
Developer 1 <developer1@GaiaTelcoms-Mac-mini.local>
2017-08-08 12:49:58 -0500

    Cambios

--- 3.AccediendoALaNubeConiOs/PeticionWebServicdor/PeticionWebServicdor/ViewController.swift
+++ 3.AccediendoALaNubeConiOs/PeticionWebServicdor/PeticionWebServicdor/ViewController.swift
@@ -31,38 +31,54 @@ class ViewController: UIViewController {
         let urls: String = "https://openlibrary.org/api/books?jscmd=data&format=json&bibkeys=ISBN:" + ISBN
         
         //978-84-376-0494-7"
-        guard let url = URL(string: urls) else {
-            print("Error: cannot create URL")
-            return
-        }
-        let urlRequest = URLRequest(url: url)
         
-        let session = URLSession.shared
+        let url = NSURL(string: urls)
+        let datos:NSData? = NSData(contentsOf: url! as URL)
         
-        let task = session.dataTask(with: urlRequest) {
-            (data, response, error) in
-            // check for any errors
-            guard error == nil else {
-                //self.showAlertBox(message: "A ocurrido un error de conexion")
-                self.lblResultado.text = "A ocurrido un error de conexion"
-                return
-            }
-            
-            guard let data = data else {
+        if (datos != nil) {
+            let texto = NSString(data: datos! as Data, encoding: String.Encoding.utf8.rawValue)
+            if (texto?.length)! <= 2 {
                 self.lblResultado.text = "No se ha encontrado ningun resultado para el ISBN: " + ISBN
-                return
-            }
-            
-            let resultado = NSString(data: data, encoding: String.Encoding.utf8.rawValue)
-            
-            if (resultado?.length)! <= 2 {
-            self.lblResultado.text = "No se ha encontrado ningun resultado para el ISBN: " + ISBN
             }
             else {
-            self.lblResultado.text = resultado! as String
+                self.lblResultado.text = texto! as String
             }
+        } else {
+            lblResultado.text = "No hay conexión a Internet"
         }
-        task.resume()
+        
+//        guard let url = URL(string: urls) else {
+//            print("Error: cannot create URL")
+//            return
+//        }
+//        let urlRequest = URLRequest(url: url)
+//        
+//        let session = URLSession.shared
+//        
+//        let task = session.dataTask(with: urlRequest) {
+//            (data, response, error) in
+//            // check for any errors
+//            guard error == nil else {
+//                //self.showAlertBox(message: "A ocurrido un error de conexion")
+//                self.lblResultado.text = "A ocurrido un error de conexion"
+//                return
+//            }
+//            
+//            guard let data = data else {
+//                self.lblResultado.text = "No se ha encontrado ningun resultado para el ISBN: " + ISBN
+//                return
+//            }
+//            
+//            let resultado = NSString(data: data, encoding: String.Encoding.utf8.rawValue)
+//            
+//            if (resultado?.length)! <= 2 {
+//                self.lblResultado.text = "No se ha encontrado ningun resultado para el ISBN: " + ISBN
+//            }
+//            else {
+//                self.lblResultado.text = resultado! as String
+//            }
+//        }
+//        task.resume()
     }
     
     func showAlertBox(message: String){
