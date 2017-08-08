155dd96fa6a8000090ce3a8efe5e69bd3dadc991
Developer 1 <developer1@GaiaTelcoms-Mac-mini.local>
2017-08-08 12:03:22 -0500

    Curso de accediendo a la nube con iOS

---
+++ 3.AccediendoALaNubeConiOs/PeticionWebServicdor/PeticionWebServicdor/ViewController.swift
@@ -0,0 +1,69 @@
+//
+//  ViewController.swift
+//  PeticionWebServicdor
+//
+//  Created by Developer 1 on 8/2/17.
+//  Copyright © 2017 comDeveloper. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var lblResultado: UILabel!
+    @IBOutlet weak var txtISBN: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func SearchingISBN(_ sender: UITextField, forEvent event: UIEvent) {
+        
+        let ISBN = txtISBN.text!
+        
+        let urls: String = "https://openlibrary.org/api/books?jscmd=data&format=json&bibkeys=ISBN:" + ISBN
+        
+        //978-84-376-0494-7"
+        guard let url = URL(string: urls) else {
+            print("Error: cannot create URL")
+            return
+        }
+        let urlRequest = URLRequest(url: url)
+        
+        let session = URLSession.shared
+        
+        let task = session.dataTask(with: urlRequest) {
+            (data, response, error) in
+            // check for any errors
+            guard error == nil else {
+                self.lblResultado.text = "A ocurrido un error de conexion"
+                return
+            }
+            
+            guard let data = data else {
+                self.lblResultado.text = "No se ha encontrado ningun resultado para el ISBN: " + ISBN
+                return
+            }
+            
+            let resultado = NSString(data: data, encoding: String.Encoding.utf8.rawValue)
+            
+            if (resultado?.length)! <= 2 {
+            self.lblResultado.text = "No se ha encontrado ningun resultado para el ISBN: " + ISBN
+            }
+            else {
+            self.lblResultado.text = resultado! as String
+            }
+        }
+        task.resume()
+        
+        
+    }
+
+}
+
